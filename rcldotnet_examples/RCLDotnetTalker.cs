6c773ad1b464c1071daa8fb45496925e057131a5
Alec Tutin <a.tutin@qut.edu.au>
2023-10-03 12:53:52 +1000

    Finished integration of timers.

--- rcldotnet_examples/RCLDotnetTalker.cs
+++ rcldotnet_examples/RCLDotnetTalker.cs
@@ -1,38 +1,37 @@
 using System;
-using System.Threading;
 using ROS2;
 
 namespace ConsoleApplication
 {
     public class RCLDotnetTalker
     {
-        private readonly Node node;
-        private readonly Publisher<std_msgs.msg.String> chatterPub;
+        private readonly Node _node;
+        private readonly Publisher<std_msgs.msg.String> _chatterPub;
 
-        private int i = 0;
-        std_msgs.msg.String msg = new();
+        private int _i = 0;
+        private readonly std_msgs.msg.String _msg = new();
 
         private RCLDotnetTalker()
         {
             RCLdotnet.Init();
-            node = RCLdotnet.CreateNode("talker");
-            node.DeclareParameter("publish_string_prefix", "Hello World");
+            _node = RCLdotnet.CreateNode("talker");
+            _node.DeclareParameter("publish_string_prefix", "Hello World");
 
-            chatterPub = node.CreatePublisher<std_msgs.msg.String>("chatter");
+            _chatterPub = _node.CreatePublisher<std_msgs.msg.String>("chatter");
 
-            ROS2.Timer timer = node.CreateTimer(new Duration(1.0), PublishChatter);
+            _node.CreateTimer(new Duration(1.0), PublishChatter);
         }
 
         private void PublishChatter()
         {
-            msg.Data = $"{node.GetParameter("publish_string_prefix").StringValue}: {i}";
-            Console.WriteLine($"Publishing: \"{msg.Data}\"");
-            chatterPub.Publish(msg);
+            _msg.Data = $"{_node.GetParameter("publish_string_prefix").StringValue}: {_i}";
+            Console.WriteLine($"Publishing: \"{_msg.Data}\"");
+            _chatterPub.Publish(_msg);
 
-            i++;
+            _i++;
         }
 
-        private void Spin() => RCLdotnet.Spin(node);
+        private void Spin() => RCLdotnet.Spin(_node);
 
         public static void Main(string[] args)
         {
